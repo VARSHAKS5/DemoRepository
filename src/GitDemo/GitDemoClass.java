88525cedb4d8feae5ad7747f865078df5bbb8d1b
K S <varsha.k-s@capgemini.com>
2024-12-17 16:17:32 +0530

    first

---
+++ src/GitDemo/GitDemoClass.java
@@ -0,0 +1,11 @@
+package GitDemo;
+
+public class GitDemoClass {
+	public static void main(String args[]) {
+		
+
+	System.out.println("Hi from STS");
+	System.out.println("Hi from GITHUB");
+	}
+
+}
