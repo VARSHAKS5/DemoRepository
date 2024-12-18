71500d9344db29cd2b03feef02b5632db9187027
K S <varsha.k-s@capgemini.com>
2024-12-18 09:58:12 +0530

    Resolved changes in GitDemoClass.java

--- src/GitDemo/GitDemoClass.java
+++ src/GitDemo/GitDemoClass.java
@@ -1,12 +1,12 @@
 package GitDemo;
 
+
 public class GitDemoClass {
-	public static void main(String[] args) {
-		
+    public static void main(String[] args) {
 
-	System.out.println("Hi from STS");
-	System.out.println("Hi from just GITHUB");
-	System.out.println("Hi from STS from first branch");
-	}
-
+        System.out.println("Hi from STS");
+        System.out.println("Hi -1 from STS");
+        System.out.println("Hi from just GITHUB");
+        System.out.println("Hi from STS from first branch");
+    }
 }
