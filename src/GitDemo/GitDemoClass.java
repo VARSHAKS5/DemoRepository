c039814a8dbc6dbdd8c4e9116856867cd67f2de3
K S <varsha.k-s@capgemini.com>
2024-12-18 09:34:48 +0530

    fourth commit

--- src/GitDemo/GitDemoClass.java
+++ src/GitDemo/GitDemoClass.java
@@ -4,8 +4,9 @@ public class GitDemoClass {
 	public static void main(String[] args) {
 		
 
-	System.out.println("Hi -1 from STS");
-	System.out.println("Hi-1 from just GITHUB");
+	System.out.println("Hi from STS");
+	System.out.println("Hi from just GITHUB");
+	System.out.println("Hi from STS from first branch");
 	}
 
 }
